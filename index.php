20c7ff4359a1954982fb7a243f9438e6d108a6d7
sarriaroman <agustin478@gmail.com>
2010-09-03 10:39:22 -0300

    Custom functiones deleted

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
 	/**
-	*	Index for CVC La voz
-	*
-	*	Capsule mode ON xD
+         *
+         *	Cronos-Aion
+	 *
 	*/
 
 	require_once( dirname(__FILE__) . "/engine/start.php" );
         
         if( $_GET['url'] == "sysinfo" ) {
             include_once( dirname(__FILE__) . "/engine/phpinfo.php" );
-        } else if( $_GET['url'] == "internalaccounts" ) {
-            include_once( dirname(__FILE__) . "/backend/accounts.php" );
         } else {
             include_once( dirname(__FILE__) . "/" . template_dir . "/index.php" );
         }
